268d304ac14c7ce18e687e91f08dc647cec0554d
nomadic <motherbodho@yandex.ru>
2022-07-07 12:39:59 +0300

    Spell checker

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SpellCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Location = new System.Drawing.Point(12, 208);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(406, 335);
+            this.elementHost1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(459, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 78);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Открыть файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_open_file_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 555);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.elementHost1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private System.Windows.Forms.Button button1;
+
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SpellCheck
+{  
+
+    public partial class Form1 : Form
+    {
+        /*Тут идёт проверка орфографии через свойство SpellCheck текстбокса из WPF (не из Win Forms). 
+         * Для импорта впф-текстбокса мы подключили в References библиотеки:
+         *  System.Xaml
+         *  WindowsFormsIntegration
+         *  PresentationCore
+         *  PresentationFramework.
+         *  В ElementHost -- контейнер для впф-элементов --  мы добавляем текстбокс из впф.
+         *  */
+        System.Windows.Controls.TextBox textBox1 = new System.Windows.Controls.TextBox();
+        string file_content;
+        string file_path;
+        OpenFileDialog file_dialog = new OpenFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+            file_dialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            this.textBox1.SpellCheck.IsEnabled = true;
+            this.textBox1.SpellCheck.CustomDictionaries.Add(new Uri("../lexicon.lex", UriKind.Relative));
+            this.elementHost1.Child = this.textBox1;
+        }
+        private void button_open_file_Click(object sender, EventArgs e)
+        {
+            if (this.file_dialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            this.file_path = this.file_dialog.FileName;
+            this.file_content = System.IO.File.ReadAllText(this.file_path);
+            this.textBox1 = new System.Windows.Controls.TextBox();
+            this.textBox1.SpellCheck.IsEnabled = true;
+            this.textBox1.Text = this.file_content;
+            this.elementHost1.Child = this.textBox1;
+            MessageBox.Show("Успешно!");
+        }
+    }
+}
